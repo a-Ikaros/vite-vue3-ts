5ad83b94be56857379f0dcf47e7c613c92451c70
a-Ikaros <873478583@qq.com>
2022-04-18 23:46:02 +0800

    Bus事件

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Main />
+</template>
+
+<script lang="ts" setup>
+import Main from './components/Main.vue'
+</script>
+
+<style lang="stylus">
+#app {
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  box-sizing border-box
+  position relative
+  width 100%
+  height 100%
+}
+</style>
